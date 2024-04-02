4b6bfe7a303ddd0f6ee340367ceda98c45c9d2fa
Johan Thor <johanthor@icloud.com>
2024-04-02 09:17:04 +0200

    Fix indentation for LaTeX

--- config.el
+++ config.el
@@ -485,6 +485,7 @@
 
 
 ;; LaTeX support:
+
 (message "LaTeX...")
 (defvar TeX-auto-save)
 (defvar TeX-parse-self)
@@ -492,6 +493,67 @@
 (defvar TeX-engine)
 (defvar TeX-view-program-selection)
 (defvar TeX-view-program-list)
+(defvar LaTeX-item-indent)
+(defvar LaTeX-indent-level-item-continuation)
+
+
+
+(defun LaTeX-indent-item ()
+  "Provide proper indentation for LaTeX \"itemize\",\"enumerate\", and
+\"description\" environments.
+
+  \"\\item\" is indented `LaTeX-indent-level' spaces relative to
+  the the beginning of the environment.
+
+  Continuation lines are indented either twice
+  `LaTeX-indent-level', or `LaTeX-indent-level-item-continuation'
+  if the latter is bound."
+  (save-match-data
+    (let* ((offset LaTeX-indent-level)
+           (contin (or (and (boundp 'LaTeX-indent-level-item-continuation)
+                            LaTeX-indent-level-item-continuation)
+                       (* 2 LaTeX-indent-level)))
+           (re-beg "\\\\begin{")
+           (re-end "\\\\end{")
+           (re-env "\\(itemize\\|\\enumerate\\|description\\)")
+           (indent (save-excursion
+                     (when (looking-at (concat re-beg re-env "}"))
+                       (end-of-line))
+                     (LaTeX-find-matching-begin)
+                     (current-column))))
+      (cond ((looking-at (concat re-beg re-env "}"))
+             (or (save-excursion
+                   (beginning-of-line)
+                   (ignore-errors
+                     (LaTeX-find-matching-begin)
+                     (+ (current-column)
+                        (if (looking-at (concat re-beg re-env "}"))
+                            contin
+                          offset))))
+                 indent))
+             ((looking-at (concat re-end re-env "}"))
+              indent)
+            ((looking-at "\\\\item")
+             (+ offset indent))
+            (t
+             (+ contin indent))))))
+
+(defcustom LaTeX-indent-level-item-continuation 4
+  "*Indentation of continuation lines for items in itemize-like
+environments."
+  :group 'LaTeX-indentation
+  :type 'integer)
+
+(eval-after-load "latex"
+  '(setq LaTeX-indent-environment-list
+         (nconc '(("itemize" LaTeX-indent-item)
+                  ("enumerate" LaTeX-indent-item)
+                  ("description" LaTeX-indent-item))
+                LaTeX-indent-environment-list)))
+
+
+
+
 
 (use-package auctex
   :ensure auctex
@@ -502,6 +564,9 @@
         TeX-parse-self t
         TeX-master nil  ; Query for the master file
         TeX-engine 'xetex)
+        ;; LaTeX-item-indent 0
+        ;; LaTeX-indent-level-item-continuation 8) ; Default is -2
+  ;; (setq LaTeX-item-indent 0) ; Default is -2
   (add-hook 'LaTeX-mode-hook 'visual-line-mode)
   (add-hook 'LaTeX-mode-hook 'flyspell-mode)
   (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
